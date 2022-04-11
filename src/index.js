9625409ba95f74521c4bb37e08ea952efbbd4cbc
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-12 00:27:48 +0300

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './style.css';
+
 function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = "Hello Webpack";
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  
+  const element = document.createElement('div');
+
+  element.innerHTML = 'Hello Webpack';
+
+  return element;
+}
+
+document.body.appendChild(component());
