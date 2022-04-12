7ff03c4cf3cb9627d52e3e495cfd6e122dfebcc2
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-12 21:06:15 +0300

    Movie popup details

--- src/modules/API.js
+++ src/modules/API.js
@@ -4,4 +4,11 @@ const getMovie = async (id) => {
   return data;
 };
 
+const getMovieDetails = async (id) => {
+  const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const data = await response.json();
+  return data;
+};
+
 export default getMovie;
+export { getMovieDetails };
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,4 +1,61 @@
-import getMovie from './API.js';
+import getMovie, { getMovieDetails } from './API.js';
+
+const showMovieDetails = (id) => {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('popup-wrapper');
+  wrapper.id = 'popup';
+
+  const popup = document.createElement('div');
+  popup.classList.add('popup');
+
+  const closeBtn = document.createElement('button');
+  closeBtn.type = 'button';
+  closeBtn.classList.add('close-btn');
+  closeBtn.innerHTML = '<i class="fa fa-close"></i>';
+  closeBtn.onclick = () => {
+    const popupShowing = document.getElementById('popup');
+    document.body.removeChild(popupShowing);
+  };
+  popup.appendChild(closeBtn);
+
+  const loadingdiv = document.createElement('div');
+  loadingdiv.innerText = 'loading';
+  popup.appendChild(loadingdiv);
+  getMovieDetails(id).then(
+    (movie) => {
+      loadingdiv.innerText = '';
+      const movieImage = document.createElement('img');
+      movieImage.alt = movie.name;
+      movieImage.src = movie.image.original;
+      popup.appendChild(movieImage);
+
+      const title = document.createElement('h4');
+      title.innerText = movie.name;
+      popup.appendChild(title);
+
+      const desc = document.createElement('div');
+      desc.innerHTML = movie.summary;
+      popup.appendChild(desc);
+
+      const detailsDiv = document.createElement('div');
+      detailsDiv.classList.add('details');
+
+      const listOne = document.createElement('ul');
+      listOne.classList.add('list-items');
+      listOne.innerHTML = `<li>Language: ${movie.language}</li> <li>runtime: ${movie.averageRuntime} minutes</li>`;
+      detailsDiv.appendChild(listOne);
+
+      const listTwo = document.createElement('ul');
+      listTwo.classList.add('list-items');
+      listTwo.innerHTML = `<li>Rating: ${movie.rating.average}/10</li> <li>premiered: ${movie.premiered}</li>`;
+      detailsDiv.appendChild(listTwo);
+
+      popup.appendChild(detailsDiv);
+      wrapper.appendChild(popup);
+      document.body.insertAdjacentElement('afterbegin', wrapper);
+    },
+  );
+};
 
 const display = async (id) => {
   const movie = await getMovie(id);
@@ -25,6 +82,7 @@ const display = async (id) => {
   const commentsBtn = document.createElement('button');
   commentsBtn.classList.add('comments-btn');
   commentsBtn.innerText = 'Comments';
+  commentsBtn.onclick = (event) => { event.preventDefault(); showMovieDetails(id); };
   card.appendChild(commentsBtn);
 };
 
