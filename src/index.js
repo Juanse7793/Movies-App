15b0f27924a8b88b6e081ad9931a43606c27513f
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-14 00:15:16 +0300

    improve popup style

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import createCards from './modules/functions.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   createCards();
-  // const itemContainer = document.getElementById('items-container');
-  // const itemTitle = document.createElement('p');
-  // itemTitle.classList.add('item-title');
-  // itemTitle.innerText = `Total Movies: ${await createCards()}`;
-  // itemContainer.appendChild(itemTitle);
 });
