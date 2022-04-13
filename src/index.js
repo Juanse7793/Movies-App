3e7af881900cea5b7d797627970241b88bbf9b50
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-04-13 12:36:30 -0500

    fixing the duplicate movies

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import createCards from './modules/functions.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   createCards();
-  const itemContainer = document.getElementById('items-container');
-  const itemTitle = document.createElement('p');
-  itemTitle.classList.add('item-title');
-  itemTitle.innerText = `The number of movies displayed is: ${createCards()}`;
-  itemContainer.appendChild(itemTitle);
 });
